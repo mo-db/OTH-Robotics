865853160fe4faae18d85e7f9acc87d5ee7f9028
Moritz Huber <104739700+mo-db@users.noreply.github.com>
2025-10-22 14:40:58 +0200

    Update sensor_click.c

--- Ex2/sensor_click.c
+++ Ex2/sensor_click.c
@@ -1,5 +1,7 @@
 int main() {
+    sensor_touch_clicked_t last_touch = SensorTouch_released;
     sensor_touch_clicked_t touch;
+    bool touch_sensor_pressed = false;
     SensorConfig(Port_0, SensorTouch);
     while(1) {
        Touch_Clicked(Port_0, &touch);
@@ -7,5 +9,10 @@ int main() {
           NNXT_LCD_DisplayStringAtLine(0, "Taster gedrueckt");
         else
           NNXT_LCD_DisplayStringAtLine(0, "Taster losgelassen");
+        if (touch != last_touch) {
+            touch_sensor_pressed = true;
+        } else {
+            touch_sensor_pressed = false;
+        }
     }
 }
