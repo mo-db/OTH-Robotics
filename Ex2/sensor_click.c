6c99aebadeb68af4c652f77bd5a24b04fc65b80c
Moritz Huber <104739700+mo-db@users.noreply.github.com>
2025-10-22 14:58:11 +0200

    Update sensor_click.c

--- Ex2/sensor_click.c
+++ Ex2/sensor_click.c
@@ -1,9 +1,14 @@
+uint32_t get_time_passed_ms(uint32_t button_press_start_ms) {
+    // return get_time() - start time
+}
+
 int main() {
     sensor_touch_clicked_t last_touch_state = SensorTouch_released;
     sensor_touch_clicked_t touch_state;
     bool touch_state_change = false;
 
     uint16_t button_press_counter = 0;
+    uint32_t button_press_start_ms = 0;
     
     SensorConfig(Port_0, SensorTouch);
     while(1) {
@@ -13,6 +18,9 @@ int main() {
         else
           NNXT_LCD_DisplayStringAtLine(0, "Button Up");
         if (touch != last_touch) {
+            // start mesuring
+            // if time_passed > x seconds, print to screen
+            // stop mesuring or restart
             button_press_counter++;
             touch_sensor_pressed = true;
         } else {
