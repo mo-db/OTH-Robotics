6d1075508277ed9a67a4939851d4cfdf67eca1f5
Moritz Huber <104739700+mo-db@users.noreply.github.com>
2025-10-30 16:40:13 +0100

    Update sensor_click.c

--- Ex2/sensor_click.c
+++ Ex2/sensor_click.c
@@ -1,58 +1,62 @@
-uint8_t move_button_pressed = 0;
-uint8_t turn_button_pressed = 0;
+#include "nnxt.h"
+#include <stdio.h>
 
-//uint8_t check_pressed(sensor_ports_e port) {
-//}
+// TODO: button presses not recognized allways
 
-void move_button() {
-    sensor_touch_clicked_t last_button_state = SensorTouch_released;
+// volatile disables compiler optimization
+volatile uint8_t button_one_pressed = 0;
+volatile uint8_t button_two_pressed = 0;
+
+
+void check_button_one() {
     sensor_touch_clicked_t button_state = SensorTouch_released;
+    sensor_touch_clicked_t last_button_state = SensorTouch_released;
 
     while(1) {
-        Delay(100);
         last_button_state = button_state;
         Touch_Clicked(Port_0, &button_state);
         if (last_button_state == SensorTouch_released && button_state == SensorTouch_clicked) {
-            move_button_pressed = 1;
-        } else {
-            move_button_pressed = 0;
+            button_one_pressed = 1;
         }
+        Delay(100);
     }
 }
 
-void turn_button() {
-    sensor_touch_clicked_t last_button_state = SensorTouch_released;
+
+void check_button_two() {
     sensor_touch_clicked_t button_state = SensorTouch_released;
+    sensor_touch_clicked_t last_button_state = SensorTouch_released;
 
     while(1) {
-        Delay(100);
         last_button_state = button_state;
-        Touch_Clicked(Port_3, &button_state);
+        Touch_Clicked(Port_1, &button_state);
         if (last_button_state == SensorTouch_released && button_state == SensorTouch_clicked) {
-            turn_button_pressed = 1;
-        } else {
-            turn_button_pressed = 0;
+            button_two_pressed = 1;
         }
+        Delay(100);
     }
-
 }
+
 
 void do_action() {
     MotorPortInit(Port_A);
     MotorPortInit(Port_B);
     motor_dir_t motorDir = Motor_dir_forward;
 
-
     while(1) {
-        if (move_button_pressed) {
+        if (button_one_pressed) {
+            button_one_pressed = 0;
+            Motor_Drive(Port_A, motorDir, 35);
+            Motor_Drive(Port_B, motorDir, 35);
+            Delay(1000);
+            Motor_Stop(Port_A, Motor_stop_float);
+            Motor_Stop(Port_B, Motor_stop_float);
 
-        } else if (turn_button_pressed) {
-            if (motorDir == Motor_dir_backward) {
-                motorDir = Motor_dir_forward;
-            } else {
-                motorDir = Motor_dir_backward;
-            }
-
+        }
+        else if (button_two_pressed) {
+            button_two_pressed = 0;
+            motorDir = (motorDir == Motor_dir_forward) ?
+                Motor_dir_backward : Motor_dir_forward;
         }
     }
 }
@@ -60,10 +64,13 @@ void do_action() {
 
 int main() {
     SensorConfig(Port_0, SensorTouch);
-    SensorConfig(Port_3, SensorTouch);
+    SensorConfig(Port_1, SensorTouch);
 
-    CreateAndStartTask(move_button);
-    CreateAndStartTask(turn_button);
+    CreateAndStartTask(check_button_one);
+    CreateAndStartTask(check_button_two);
+    CreateAndStartTask(do_action);
+
+    printf("Tasks were started\n");
 
     StartScheduler();
 
